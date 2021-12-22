fcf256b46d60931bd989dec9801fe95872069207
Nir Graziani <nirg6888@gmail.com>
2021-12-22 11:57:47 +0200

    add mongoose

--- server.js
+++ server.js
@@ -34,31 +34,43 @@ const posts = [
   { id: 13, name: "Post 13" }
 ];
 
-app.get("/users", (req, res) => {
-  const page = parseInt(req.query.page);
-  const limit = parseInt(req.query.limit);
-
-  const startIndex = (page - 1) * limit;
-  const endIndex = page * limit;
-
-  const results = {};
-
-  if (endIndex < users.length) {
-    results.next = {
-      page: page + 1,
-      limit: limit
-    };
-  }
-
-  if (startIndex > 0) {
-    results.previous = {
-      page: page - 1,
-      limit: limit
-    };
-  }
-
-  results.results = users.slice(startIndex, endIndex);
-  res.json(results);
+app.get("/posts", paginatedResults(posts), (req, res) => {
+  res.json(res.paginatedResults);
 });
+
+app.get("/users", paginatedResults(users), (req, res) => {
+  res.json(res.paginatedResults);
+});
+
+function paginatedResults(model) {
+  return (req, res, next) => {
+    const page = parseInt(req.query.page);
+    const limit = parseInt(req.query.limit);
+
+    const startIndex = (page - 1) * limit;
+    const endIndex = page * limit;
+
+    const results = {};
+
+    if (endIndex < model.length) {
+      results.next = {
+        page: page + 1,
+        limit: limit
+      };
+    }
+
+    if (startIndex > 0) {
+      results.previous = {
+        page: page - 1,
+        limit: limit
+      };
+    }
+
+    results.results = model.slice(startIndex, endIndex);
+
+    res.paginatedResults = results;
+    next();
+  };
+}
 
 app.listen(3001);
